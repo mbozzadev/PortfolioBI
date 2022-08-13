15320f80a0a2f11ba8bdf5e0f55f75021cb19097
mbozzadev <michaelbbozzadev@gmail.com>
2022-08-13 18:10:36 -0400

    Create Shell

---
+++ PortfolioBI_Console/PortfolioBI_Console/PBI_Driver.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortfolioBI_Console
+{
+    internal class PBI_Driver
+    {
+
+        //Create 2 tables: DataFileNames / DataFileContents
+        //DataFileNames : CorningData / NvidiaData / CreationDate
+        //DataFileContents : All data from each file, connected by a pointer/file name / CreationDate
+        //   - Pointer would be better in case the same file name is submitted more than once
+
+        internal void BeginPBIProcessingFromLocalFile()
+        {
+            Console.WriteLine("Processing PBI");
+
+            string dataStagingPath = "C:\\Users\\mikeb\\Desktop\\Dev Projects\\Docs\\";
+            string corningDataFile = "Corning_CLW_2015Data";
+            string nvidiaDataFile = "Nvidia_NVDA_2015Data";
+
+
+
+        }
+
+        //This process would have a front end loader to read the data into a table/queue, which could then be pulled from this project
+        internal void BeginPBIProcessingFromDatabase()
+        {
+
+        }
+
+    }
+}
---
+++ PortfolioBI_Console/PortfolioBI_Console/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortfolioBI_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting PortfolioBI App");
+            try
+            {
+                PBI_Driver pbi = new PBI_Driver();
+                pbi.BeginPBIProcessing();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR: Could not process PortfolioBI properly");
+                Console.WriteLine("ERROR: " + ex.ToString());
+            }
+
+
+            Console.WriteLine("Closing PortfolioBI App");
+        }
+    }
+}
